5561b922465942643af7b73f201f98563f1fa34e
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-05-23 23:56:16 +0100

    change page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
-        MedConnect
+        This site is under construction.
       </h1>
     </main>
   )
